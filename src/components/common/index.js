c7be607997a0df0c503d74064d6a59194b1f379e
Vladimir <vmagdesyan@yandex.ru>
2019-10-16 19:53:15 +0300

    Добавляет файл экспорта стандартных элементов

---
+++ src/components/common/index.js
@@ -0,0 +1,7 @@
+export { Title } from './Title';
+export { Preloader } from './Preloader';
+export { Link } from './Link';
+export { Input } from './Input';
+export { Button } from './Button';
+export { BackButton } from './BackButton';
+export { SectionTitle } from './SectionTitle';
